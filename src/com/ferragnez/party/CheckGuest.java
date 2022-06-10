d611c16784d42166eec52e71e5b8eb08abae3289
EmanueleEsposito <manu2022@blu.it>
2022-06-10 15:15:08 +0200

    aggiunto creazione e inizializzazione array nome degli invitati

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -3,8 +3,12 @@ package com.ferragnez.party;
 public class CheckGuest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//Creazione e inizializzazione array contennte nome degli invita
+		String[] nomiDegliInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi","Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"
+		};
+		for (int i=0; i < nomiDegliInvitati.length; i++) {
+			System.out.println(nomiDegliInvitati[i]);
+		}
 	}
 
 }
